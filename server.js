5af2fa7fabfccb1c0ef3098b6245bc583b5ad46b
chupacabra <ryan.russell001@yahoo.com>
2016-08-12 18:07:44 -0700

    reformat server io calls, time is working

--- server.js
+++ server.js
@@ -4,89 +4,64 @@ var express = require('express');
 var socketio = require('socket.io');
 var os = require('os');
 var si = require('systeminformation');
+var moment = require('moment');
 
 var server = express();
 server.use('/', express.static(__dirname + '/'));
 var io = socketio(server.listen(process.env.PORT || 8080));
 
-si.getStaticData()
-  .then(data => {
-    console.log('STATIC DATA');
-    console.log(data);
+//si.getStaticData()
+//  .then(data => {
+//    console.log('STATIC DATA');
+//    console.log(data);
+//  });
+//
+//si.getDynamicData()
+//  .then(data => {
+//    console.log();
+//    console.log('DYNAMNIC DATA');
+//    console.log(data);
+//  });
+//
+//si.getAllData()
+//  .then(data => {
+//    console.log();
+//    console.log('ALL DATA');
+//    console.log(data);
+//  });
+
+var cpu, os = 0;
+var clients = 0;
+si.cpu()
+  .then(cpuData => {
+    cpu = cpuData;
+  })
+
+si.osInfo()
+  .then(osData => {
+    os = osData
+  })
+
+io.on('connection', function(socket) {
+  ++clients;
+  socket.emit('onConnection', {
+    osInfo: os,
+    cpuInfo: cpu
   });
-
-si.getDynamicData()
-  .then(data => {
-    console.log();
-    console.log('DYNAMNIC DATA');
-    console.log(data);
-  });
-
-si.getAllData()
-  .then(data => {
-    console.log();
-    console.log('ALL DATA');
-    console.log(data);
-  });
-
-var sendError = function(event, error) {
-  io.emit(event, {
-    err: error
-  });
-}
-
-io.on('connection', function() {
-  io.emit('hostData', {
-    hostname: os.hostname(),
-    arch: os.arch(),
-    type: os.type(),
-    plat: os.platform(),
-    rel: os.release(),
-    user: os.userInfo()
-  });
-
-  si.cpu()
-    .then(cpu => {
-      io.emit('cpu', {
-        man: cpu.manufacturer,
-        brand: cpu.brand,
-        speed: cpu.speed,
-        cores: cpu.cores
-      });
-    })
-    .catch(error => {
-      sendError('cpu', error);
-    });
-
-  si.osInfo()
-    .then(os => {
-      io.emit('os', {
-        plat: os.platform,
-        distro: os.distro,
-        release: os.release,
-        cname: os.codename,
-        kernel: os.kernel,
-        arch: os.arch,
-        host: os.hostname,
-        lgfile: os.logofile
-      });
-    })
-    .catch(error => {
-      sendError('os', error);
-    });
-
-  setInterval(function() {
-    //{
-    //  freemem: os.freemem(),
-    //  load: os.loadavg(),
-    //  net: os.networkInterfaces(),
-    //  totalmem: os.totalmem(),
-    //  uptime: os.uptime()
-    //});
-  }, 1000);
 });
 
-var time = function(timeSeconds) {
+setInterval(function() {
+  io.emit('time', {
+    curr: functionParseDate(si.time().current),
+    uptime: functionTime(si.time().uptime)
+  });
+}, 1000);
+
+io.on('disconnect', function() {
+  --clients;
+});
+
+var functionTime = function(timeSeconds) {
   var date = new Date(timeSeconds * 1000);
   var h = date.getUTCHours();
   var m = date.getUTCMinutes();
@@ -99,3 +74,8 @@ var time = function(timeSeconds) {
   return h + ":" + m + ":" + s;
 };
 
+var functionParseDate = function(longEpoch) {
+  var epochDate = moment(longEpoch);
+  var strDate = epochDate.format("dddd, DD MMMM, YYYY HH:mm:ss");
+  return strDate;
+};
